38a9799c382715ce41915d5886ea3928ada3a5c2
Huiying Wang <whymissu1228@gmail.com>
2023-01-06 16:01:46 -0800

    added sent message js file

---
+++ sendEmail.js
@@ -0,0 +1,24 @@
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "whymissu1228@gmail.com",
+        Password: "8CF12C0FC4A6D3D56D8C43A5E9920DF65099",
+        To: 'whymissu1228@gmail.com',
+        From: document.getElementById("yourEmail").value,
+        Subject: "From my Portfolio",
+        Body: "from: " + document.getElementById("yourName").value + "\n"
+            + document.getElementById("yourEmail").value + "\n"
+            + document.getElementById("yourMessage").value
+    }).then(
+        // message => alert(message)
+        clearThis(document.getElementById("yourName")),
+        clearThis(document.getElementById("yourEmail")),
+        clearThis(document.getElementById("yourMessage")),
+        document.getElementById("yourSubmit").value = "Successful sent",
+        document.getElementById("yourSubmit").style.color = "rgb(155, 102, 102)"
+    );
+}
+function clearThis(target) {
+    target.value= "";
+}
